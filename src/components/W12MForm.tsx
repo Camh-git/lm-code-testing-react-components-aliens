f87c49a9f10e388df8444e010d1d85707fd09324
Camh-git <cameronhillier1@gmail.com>
2023-09-11 22:54:50 +0100

    Activity 2 and part of 3
    read through activity 2
    implemented species component of activity 3, added it to the form and improved it using the hints

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react';
-import W12MHeader from './W12MHeader';
+import { useState } from "react";
+import W12MHeader from "./W12MHeader";
+import W12Mspecies from "./W12Mspecies";
 
 const W12MForm = () => {
-	return (
-		<section className='w12MForm'>
-			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
-		</section>
-	);
+  const [speciesName, setSpeciesName] = useState("humans");
+
+  return (
+    <section className="w12MForm">
+      <W12MHeader />
+      <W12Mspecies
+        species={speciesName}
+        onChangeSpecies={(value) => setSpeciesName(value)}
+      />
+    </section>
+  );
 };
 
 export default W12MForm;
---
+++ src/components/W12Mspecies.tsx
@@ -0,0 +1,21 @@
+interface SpeciesNameProps {
+  species: string;
+  onChangeSpecies: (value: string) => void;
+}
+
+const W12Mspecies: React.FC<SpeciesNameProps> = ({
+  species,
+  onChangeSpecies,
+}) => (
+  <>
+    <label htmlFor="species">Species Name: </label>
+    <input
+      id="species"
+      type="text"
+      value={species}
+      onChange={(e) => onChangeSpecies(e.target.value)}
+    />
+  </>
+);
+
+export default W12Mspecies;
